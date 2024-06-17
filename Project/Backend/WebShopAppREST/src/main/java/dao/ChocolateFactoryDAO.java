ab8ebd3752640df0ceef2b2e0568cad77a23a02e
Nataša Radmilović <nata.radmilovic@gmail.com>
2024-06-17 23:06:24 +0200

    Testing

--- Project/Backend/WebShopAppREST/src/main/java/dao/ChocolateFactoryDAO.java
+++ Project/Backend/WebShopAppREST/src/main/java/dao/ChocolateFactoryDAO.java
@@ -144,7 +144,7 @@ public class ChocolateFactoryDAO {
 		}
 	}
 	
-	private void saveFactory(String contextPath) {
+	public void saveFactory(String contextPath) {
 	    BufferedWriter out = null;
 	    try {
 	        File file = new File(contextPath + "/factories.txt");
