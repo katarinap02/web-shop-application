fe46287e3b57a221fcc4282228a59f27d55f059a
Nataša Radmilović <nata.radmilovic@gmail.com>
2024-06-17 22:48:44 +0200

    Added Shopping class

---
+++ Project/Backend/WebShopAppREST/src/main/java/beans/Shopping.java
@@ -0,0 +1,72 @@
+package beans;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import enums.ShoppingStatus;
+
+public class Shopping {
+
+	private String id;
+	private List<Integer> chocolateIds;
+	private int factoryId;
+	private LocalDateTime dateTime;
+	private double price;
+	private String customerName;
+	private ShoppingStatus status;
+	public Shopping(String id, List<Integer> chocolateIds, int factoryId, LocalDateTime dateTime, double price,
+			String customerName, ShoppingStatus status) {
+		super();
+		this.id = id;
+		this.chocolateIds = chocolateIds;
+		this.factoryId = factoryId;
+		this.dateTime = dateTime;
+		this.price = price;
+		this.customerName = customerName;
+		this.status = status;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public List<Integer> getChocolateIds() {
+		return chocolateIds;
+	}
+	public void setChocolateIds(List<Integer> chocolateIds) {
+		this.chocolateIds = chocolateIds;
+	}
+	public int getFactoryId() {
+		return factoryId;
+	}
+	public void setFactoryId(int factoryId) {
+		this.factoryId = factoryId;
+	}
+	public LocalDateTime getDateTime() {
+		return dateTime;
+	}
+	public void setDateTime(LocalDateTime dateTime) {
+		this.dateTime = dateTime;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getCustomerName() {
+		return customerName;
+	}
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	public ShoppingStatus getStatus() {
+		return status;
+	}
+	public void setStatus(ShoppingStatus status) {
+		this.status = status;
+	}
+	
+	
+}
